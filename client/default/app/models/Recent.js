03a3b65fc81495e2e81e4cf552b710794a55fb70
cianclarke <cian@cianclarke.com>
2011-12-10 15:05:50 +0000

    Add formattign to image

--- client/default/app/models/Recent.js
+++ client/default/app/models/Recent.js
@@ -19,7 +19,7 @@ app.stores.recent = new Ext.data.Store({
            name: '11011 for S&iacute;ol',
            medium: 'Acrylic paint on aluminium',
            size: '89" X 85" 2011',
-           image: 'images/392.jpg',
+           image: '392.jpg',
            category: 'aluminiums'
            
          },
@@ -27,23 +27,28 @@ app.stores.recent = new Ext.data.Store({
            name: '7811',
            medium: 'Acrylic paint on aluminium', 
            size: '99" X 72" Aug 2011',
-           image: 'images/391.jpg',
+           image: '391.jpg',
            category: 'aluminiums'
-
          },
          {
            name: 'RVR',
            medium: 'Acrylic paint on aluminium',
            size: '89" X 49"',
-           image: 'images/393.jpg',
+           image: '393.jpg',
            category: 'aluminiums'
-
+         },
+         {
+           name: 'Accord',
+           medium: 'Ground aluminium and acrylic medium',
+           size: '19" X 15", 2011',
+           image: '394.jpg',
+           category: 'aluminiums'
          },
          {
            name: 'Line of Modigliani',
            medium: 'Solid aluminium and acrylic medium',
            size: '20" X 15", 2011',
-           image: 'images/385.jpg',
+           image: '385.jpg',
            category: 'aluminiums'
 
          },
@@ -51,10 +56,30 @@ app.stores.recent = new Ext.data.Store({
            name: 'Solo',
            medium: 'Aluminium block and acrylic medium',
            size: '19" x 15", 6-6-2011',
-           image: 'images/384.jpg',
+           image: '384.jpg',
            category: 'aluminiums'
-
          },
-         
+         {
+           name: 'Granite Paint',
+           medium: 'Acrylic on aluminium',
+           size: '19" x 15", 2011',
+           image: '380.jpg',
+           category: 'aluminiums'
+         },
+         {
+           name: 'Belfast Painting II',
+           medium: 'Acrylic on aluminum',
+           size: '13"X10", 2010-2011',
+           image: '367.jpg',
+           collection: 'Private Collection, New York, USA',
+           category: 'aluminiums'
+         },
+         {
+           name: 'For M',
+           medium: 'Acrylic on aluminium',
+           size: '20" X 16", 2011',
+           image: '383.jpg',
+           category: 'aluminiums'
+         }
   ]
 });
--- client/default/app/views/Image.js
+++ client/default/app/views/Image.js
@@ -2,12 +2,26 @@ app.views.Image = Ext.extend(Ext.Panel, {
 
   title: 'Image Details',
   cls: 'imageDetails',
-  tpl: '<img style="height: 80%" src="{image}" alt="{title}"/><br />' +
+  tpl: '<img style="height: 80%" src="http://www.ciaranlennon.com/images/lenses/{image}" alt="{title}"/><br />' +
+  '<span class="name">{name}</span><br />'+
   '{medium}<br />'+
   '{size}<br />',
   initComponent: function(){
+    var me = this;
     var container = this.attachedTo;
     var title = this.title || '';
+    
+    this.listeners =  {
+      click: {    // FIXME: Expand / collapse code here
+          element: 'el',
+          fn: function() {    // FIXME: Expand / collapse code here
+              container.setActiveItem(0, 'slide');
+              me.destroy();
+              
+          }
+      }
+    };
+    
     this.dockedItems = [
                   {
                     xtype: 'toolbar',
@@ -19,11 +33,13 @@ app.views.Image = Ext.extend(Ext.Panel, {
                                 ui: 'back',
                                 handler: function(){
                                   container.setActiveItem(0, 'slide');
+                                  me.destroy();
                                 }
                               }
                             ]
                   }
-    ]
+    ];
+    
     app.views.Image.superclass.initComponent.call(this);
   }
   
