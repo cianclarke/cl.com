37f116101168125a5b0fa2e55619bb9434ad67a8
cianclarke <cian@cianclarke.com>
2011-12-11 21:18:15 +0000

    Add code for ori change

--- client/default/app/app.js
+++ client/default/app/app.js
@@ -4,3 +4,22 @@ Ext.regApplication({
         this.views.viewport = new this.views.Viewport();
     }
 });
+
+
+$fh.ready( {}, 
+    function () {
+      // When app comes to forescreen on iOS, reload all stores from server
+      document.addEventListener("resume", function(){
+      app.stores.recent.load();
+      }, false);
+      
+      $fh.ori({
+        act: "listen"
+      }, function (a) {
+        setTimeout(function () {
+          app.views.viewport.setOrientation(Ext.getOrientation(), window.innerWidth, window.innerHeight);
+          window.scrollTo(0, 0);
+          app.views.viewport.doComponentLayout();
+        }, 100)
+});
+
