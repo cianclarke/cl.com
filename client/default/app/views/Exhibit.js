9cd5007046a11efe645afa7d36e8872277d5eeac
cianclarke <cian@cianclarke.com>
2012-01-29 15:28:32 +0000

    Make each image 100% high

--- client/default/app/views/Exhibit.js
+++ client/default/app/views/Exhibit.js
@@ -19,7 +19,7 @@ app.views.Exhibit = Ext.extend(Ext.Panel, {
     for (var i=0; i<images.length; i++){
       var image = new Ext.Panel({
         html: '<img style="" src="http://www.ciaranlennon.com/' + images[i] + '" alt="Exhibition Image">',
-        flex: 1
+        height: '100%',
       });  
       imagePanels.push(image);
     }
