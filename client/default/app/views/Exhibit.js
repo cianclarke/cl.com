5acded9b27635768cdaae1369a4f67c6eda49039
cianclarke <cian@cianclarke.com>
2012-01-29 17:07:53 +0000

    Make exhibit instance flex height

--- client/default/app/views/Exhibit.js
+++ client/default/app/views/Exhibit.js
@@ -19,7 +19,8 @@ app.views.Exhibit = Ext.extend(Ext.Panel, {
     for (var i=0; i<images.length; i++){
       var image = new Ext.Panel({
         html: '<img style="" src="http://www.ciaranlennon.com/' + images[i] + '" alt="Exhibition Image">',
-        height: '100%',
+        flex: 1
+        //height: '100%',
       });  
       imagePanels.push(image);
     }
