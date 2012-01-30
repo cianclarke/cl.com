2b172686e06ab63acc48c76c8339980392ab8f14
cianclarke <cian@cianclarke.com>
2012-01-30 00:34:25 +0000

    Load all stores on new data

--- client/default/app/app.js
+++ client/default/app/app.js
@@ -11,6 +11,8 @@ $fh.ready( {},
       // When app comes to forescreen on iOS, reload all stores from server
       document.addEventListener("resume", function(){
       app.stores.recent.load();
+      app.stores.dailies.load();
+      app.stores.exhibitions.load();
       }, false);
       
       $fh.ori({
